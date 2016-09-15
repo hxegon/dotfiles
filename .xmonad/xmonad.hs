a7874c699dac2768ed207c99cd2eb8753076ab40
Cooper LeBrun <cooperlebrun@gmail.com>
2016-09-15 10:47:30 -0700

    extract layout, add tiled, mirror tiled, full, tabbed

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,4 +1,5 @@
 import XMonad
+import XMonad.Layout.Tabbed
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
@@ -7,11 +8,26 @@ import System.IO
 
 myTitleMaxLength = 50
 
+myLayout = avoidStruts $
+           tiled
+           ||| Mirror tiled
+           ||| Full
+           ||| tabbed shrinkText defaultTheme
+  where
+     -- default tiling algorithm partitions the screen into two panes
+     tiled   = Tall nmaster delta ratio
+     -- The default number of windows in the master pane
+     nmaster = 1
+     -- Default proportion of screen occupied by master pane
+     ratio   = 1/2
+     -- Percent of screen to increment by when resizing panes
+     delta   = 2/100
+
 main = do
   xmproc <- spawnPipe "/usr/bin/xmobar /home/clebrun/.xmobarrc"
   xmonad $ defaultConfig
     { manageHook = manageDocks <+> manageHook defaultConfig
-    , layoutHook = avoidStruts $ layoutHook defaultConfig
+    , layoutHook = myLayout
     , logHook    = dynamicLogWithPP xmobarPP
       { ppOutput = hPutStrLn xmproc
       , ppTitle  = xmobarColor "orange" "" . shorten myTitleMaxLength
