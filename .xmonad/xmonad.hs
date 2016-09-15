73852aa82ecc244801f08a5e574ed57e7ca7c13c
Cooper LeBrun <cooperlebrun@gmail.com>
2016-09-15 10:46:41 -0700

    remove old keybindings to scrot, screensaver

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -18,8 +18,4 @@ main = do
       }
     , modMask    = mod4Mask -- mod4 = Super, mod1 = left alt
     , terminal   = "gnome-terminal"
-    } `additionalKeys`
-    [ ((mod1Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
-    , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
-    , ((0, xK_Print), spawn "scrot")
-    ]
+    }
