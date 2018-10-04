21fd4f43452e134ef8056f3ff49e987685e4acfb
Cooper LeBrun <cooperlebrun@gmail.com>
2018-10-04 12:37:28 -0700

    Add hammerspoon config

---
+++ hammerspoon/.hammerspoon/init.lua
@@ -0,0 +1,35 @@
+-- Hammerspoon config by Cooper LeBrun <cooperlebrun@gmail.com>
+
+-----------
+-- SETUP --
+-----------
+launchBinds = {}
+function bindLauncher(pre, key, appname)
+  -- bind the key
+  hs.hotkey.bind(pre, key, function()
+    hs.application.launchOrFocus(appname)
+  end)
+  -- add to launchBinds
+  launchBinds[#launchBinds+1]=(key .. " => " .. appname)
+end
+
+-------------
+-- GLOBALS --
+-------------
+local prefix = {"cmd", "shift"}
+
+--------------
+-- BINDINGS --
+--------------
+bindLauncher(prefix, "m", "Messages")
+bindLauncher(prefix, "t", "iTerm")
+bindLauncher(prefix, "s", "Spotify")
+bindLauncher(prefix, "b", "Firefox")
+bindLauncher(prefix, "d", "Discord")
+bindLauncher(prefix, "r", "Reminders")
+bindLauncher(prefix, "f", "Finder")
+bindLauncher(prefix, "c", "Calendar")
+
+hs.hotkey.bind(prefix, "/", function() -- Show app launcher bindings
+  hs.alert.show("PREFIX: " .. table.concat(prefix, "+") .. "\n" .. table.concat(launchBinds, "\n"), 7)
+end)
