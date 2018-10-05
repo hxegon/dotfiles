e83a8d9269b5e6198bdae245052981fc3bcec949
Cooper LeBrun <cooperlebrun@gmail.com>
2018-10-04 18:40:28 -0700

    hammerspoon: add dynamic app shortcut bindings

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -27,6 +27,9 @@ function bindLauncher(pre, key, appname)
   launchBinds[#launchBinds+1]=(key .. " => " .. appname)
 end
 
+-- [1] is initially nil for a hotkey check branch, gets reset to application names
+-- [2] is what gets printed if there's no app set yet
+varApp = { nil, "No App bound yet, use " .. showKeys(prefix) .. "+delete to set the currently focused window" }
 
 --------------
 -- BINDINGS --
@@ -44,4 +47,18 @@ bindLauncher(prefix, "n", "Notes")
 
 hs.hotkey.bind(prefix, "/", function() -- Show app launcher bindings
   hs.alert.show("PREFIX: " .. showKeys(prefix) .. "\n" .. table.concat(launchBinds, "\n"), 7)
+end)
+
+-- Dynamic app key binding. Use with prefix+space, set with prefix+delete
+hs.hotkey.bind(prefix, "delete", function()
+  local curAppName = hs.application.name(hs.application.frontmostApplication())
+  hs.alert.show(curAppName .. " bound to " .. showKeys(prefix) .. "+space key")
+  varApp[1]=curAppName
+end)
+hs.hotkey.bind(prefix, "space", function()
+  if varApp[1] == nil then
+    hs.alert.show(varApp[2])
+  else
+    hs.application.launchOrFocus(varApp[1])
+  end
 end)
