a3aaf6fa4f6e585900f37f7a28b62c43459e945a
Cooper LeBrun <cooperlebrun@gmail.com>
2019-03-10 12:37:07 -0700

    Emacs: Add typescript config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -145,3 +145,19 @@
 
 (provide 'init)
 ;;; init ends here
+
+;; Typescript
+; http://redgreenrepeat.com/2018/05/04/typescript-in-emacs/
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  (company-mode +1))
+
+(setq company-tooltip-align-annotations t)
+
+(add-hook 'before-save-hook 'tide-format-before-saves)
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
