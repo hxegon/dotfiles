47552a637ad1f0a80254bb3910fc274550ef6205
Cooper LeBrun <cooperlebrun@gmail.com>
2019-03-12 09:39:16 -0700

    Emacs: Refine JS support

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -162,6 +162,25 @@
 (add-hook 'before-save-hook 'tide-format-before-saves)
 (add-hook 'typescript-mode-hook #'setup-tide-mode)
 
+;; Javascript (https://emacs.cafe/emacs/javascript/setup/2017/04/23/emacs-setup-javascript.html)
+(require 'js2-mode)
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+
+					; better imenu
+(add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
+
+(require 'js2-refactor)
+(require 'xref-js2)
+
+(add-hook 'js2-mode-hook #'js2-refactor-mode)
+(js2r-add-keybindings-with-prefix "C-c C-r")
+(define-key js2-mode-map (kbd "C-k") #'js2r-kill)
+
+(define-key js-mode-map (kbd "M-.") nil)
+
+(add-hook 'js2-mode-hook (lambda ()
+   (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t )))
+
 ;; END CONFIG
 
 (provide 'init)
