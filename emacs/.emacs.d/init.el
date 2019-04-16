c483d2a1e2826ef64197c9c5e1c871558ac6f9fa
Cooper LeBrun <cooperlebrun@gmail.com>
2019-04-16 11:38:16 -0700

    Emacs: Config helm replacements for M-x, C-x b, C-x C-f

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -241,6 +241,13 @@
 (require 'circe-color-nicks)
 (enable-circe-color-nicks)
 
+;; HELM
+(require 'helm)
+(helm-mode 1)
+(global-set-key (kbd "C-x b") 'helm-buffers-list)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "M-x") 'helm-M-x)
+
 ;; END CONFIG
 
 (provide 'init)
