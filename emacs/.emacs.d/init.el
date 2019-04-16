d45214e89844d0302acba30fe03187995956cb23
Cooper LeBrun <cooperlebrun@gmail.com>
2019-04-16 11:41:19 -0700

    emacs: helm replacement for recentf (C-x C-r)

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -247,6 +247,7 @@
 (global-set-key (kbd "C-x b") 'helm-buffers-list)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x C-r") 'helm-recentf)
 
 ;; END CONFIG
 
