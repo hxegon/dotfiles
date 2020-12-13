25850c6d1993b3b1966a1be02eed18c306b5f11d
Cooper LeBrun <cooperlebrun@gmail.com>
2020-12-13 01:06:36 -0800

    fish: add check for zstd to comp fn

--- fish/.config/fish/functions/comp.fish
+++ fish/.config/fish/functions/comp.fish
@@ -1,4 +1,8 @@
 function comp -a target
+  if not which zstd
+    echo 'zstd not in $PATH. Please ensure zstd is installed and available.'
+    exit 1
+  end
   if test -d $target
     # TODO: Confirm if user wants to remove source
     tar cf - $target | zstd > "$target.tar.zst" \
