6bd1d14e2bb97ca3b3b09ed8ebde0b94600b77f4
Cooper LeBrun <cooperlebrun@gmail.com>
2022-02-17 12:33:43 -0700

    fish prompt config

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,81 +1,38 @@
 function fish_prompt
-	and set retc green
-    or set retc red
-    tty | string match -q -r tty
-    and set tty tty
-    or set tty pts
+end
 
-    set_color $retc
-    if [ $tty = tty ]
-        echo -n .-
-    else
-        echo -n '┬─'
-    end
-    set_color -o green
-    echo -n [
-    if test "$USER" = root -o "$USER" = toor
-        set_color -o red
-    else
-        set_color -o yellow
-    end
-    echo -n $USER
-    set_color -o white
-    echo -n @
-    if [ -z "$SSH_CLIENT" ]
-        set_color -o blue
-    else
-        set_color -o cyan
-    end
-    echo -n (prompt_hostname)
-    set_color -o white
-    #echo -n :(prompt_pwd)
-    echo -n :(pwd|sed "s=$HOME=~=")
-    set_color -o green
-    echo -n ']'
-    set_color normal
-    set_color $retc
-    if [ $tty = tty ]
-        echo -n '-'
-    else
-        echo -n '─'
-    end
-    set_color -o green
-    echo -n '['
-    set_color normal
-    set_color $retc
-    echo -n (date +%X)
-    set_color -o green
-    echo -n ]
+status is-interactive || exit
 
-    if type -q acpi
-        if [ (acpi -a 2> /dev/null | string match -r off) ]
-            echo -n '─['
-            set_color -o red
-            echo -n (acpi -b|cut -d' ' -f 4-)
-            set_color -o green
-            echo -n ']'
-        end
+_tide_remove_unusable_items
+
+# The first element in $$_tide_prompt_var is right prompt
+# All remaining ones are 'left' prompt (also upper right in 2-line prompts)
+set -g _tide_prompt_var _tide_prompt_$fish_pid
+
+function _tide_refresh_prompt --on-variable $_tide_prompt_var
+    set -g _tide_self_repainting # prevents us from creating a second background job
+    commandline --function repaint
+end
+
+function fish_prompt
+    _tide_last_status=$status _tide_last_pipestatus=$pipestatus if not set -e _tide_self_repainting
+        jobs --query
+        fish --command "_tide_jobs_status=$status CMD_DURATION=$CMD_DURATION COLUMNS=$COLUMNS \
+            fish_bind_mode=$fish_bind_mode set -U $_tide_prompt_var (_tide_prompt)" &
+        builtin disown
+
+        command kill $_tide_last_pid 2>/dev/null
+        set -g _tide_last_pid $last_pid
     end
-    echo
-    set_color normal
-    for job in (jobs)
-        set_color $retc
-        if [ $tty = tty ]
-            echo -n '; '
-        else
-            echo -n '│ '
-        end
-        set_color brown
-        echo $job
-    end
-    set_color normal
-    set_color $retc
-    if [ $tty = tty ]
-        echo -n "'->"
-    else
-        echo -n '╰─>'
-    end
-    set_color -o red
-    echo -n '$ '
-    set_color normal
+
+    test "$tide_prompt_add_newline_before" = true && echo
+    string unescape $$_tide_prompt_var[1][2..]
+end
+
+function fish_right_prompt
+    string unescape $$_tide_prompt_var[1][1]
+end
+
+function _tide_on_fish_exit --on-event fish_exit
+    set -e $_tide_prompt_var
 end
