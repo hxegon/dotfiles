ad601ac58948083b57c020bf63058c566a8ca5eb
Cooper LeBrun <cooperlebrun@gmail.com>
2024-03-20 14:43:43 -0400

    pre-backup hyprland commit (fun while it lasted but it's so much wooooork)

--- hyprland/.config/hypr/bin/laptop_lid.sh
+++ hyprland/.config/hypr/bin/laptop_lid.sh
@@ -4,6 +4,9 @@
 if [[ $(hyprctl monitors) =~ "\sDP-[0-9+]" ]]; then
     # then close/open lid behavior only turns off display
     if [[ $1 == "open" ]]; then
+        # FIXME: Doesn't work unless the external monitor is registered
+        # as DP-1. Need to capture the actual ID of the monitor
+        # in the regex.
         hyprctl keyword monitor "eDP-1,22556x1504,0x0,1"
         MSG="Enabling laptop monitor"
     elif [[ $1 == "close" ]]; then
