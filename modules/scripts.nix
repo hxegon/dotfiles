3a546cb3953441b1732f6b360b669a7c19c446e2
Cooper LeBrun <cooperlebrun@gmail.com>
2024-03-23 17:29:13 -0400

    scripts.nix: add missing fzy dependency (needed by tms)

--- modules/scripts.nix
+++ modules/scripts.nix
@@ -1,6 +1,8 @@
 { config, lib, pkgs, ... }:
 
 {
+  # fzy is needed for the tms script
+  home.packages = [ pkgs.fzy ];
   # Not sure how to manage my scripts through nix cleanly yet,
   # but I can at least start by adding the path here.
   home.sessionPath = ["$HOME/scripts"];
