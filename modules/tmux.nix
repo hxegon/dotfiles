20b444c62778dc94a42bbd504ff940c4d679abaa
Cooper LeBrun <cooperlebrun@gmail.com>
2024-03-23 15:40:51 -0400

    migrate tmux popup bindings from .tmux.conf

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -18,6 +18,12 @@
         prefix = "C-Space";
         terminal = "screen-256color";
 
+        extraConfig = ''
+            bind-key C-o popup -h 80% -w 60% -E tms
+            bind-key C-g popup -d "#{pane_current_path}" -h 90% -w 100% -E lazygit
+            bind-key C-j popup -d "#{pane_current_path}" -h 50% -w 50% -E just --choose
+        '';
+
         plugins = with pkgs; [
             {
                 plugin = tmuxPlugins.power-theme;
