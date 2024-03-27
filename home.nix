d2f46364cd21fc8bd88d62f0951f81994e1e01c4
Cooper LeBrun <cooperlebrun@gmail.com>
2024-03-27 16:42:59 -0400

    add libreoffice, update flakes

--- home.nix
+++ home.nix
@@ -62,6 +62,10 @@
     nixfmt
     qemu
 
+    libreoffice # Productivity tools
+    # gimp # Photo editing
+    # rawtherapee # raw photo editing
+
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
@@ -75,21 +79,6 @@
     #   echo "Hello, ${config.home.username}!"
     # '')
   ];
-
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
 
   home.sessionVariables = {
     EDITOR = "nvim";
