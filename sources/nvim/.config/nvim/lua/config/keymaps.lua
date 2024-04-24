3a4ba927729751555c6022247dc240b5a3acf5f0
Cooper LeBrun <cooperlebrun@gmail.com>
2024-04-24 09:40:40 -0400

    add nvim "bubble" keymap

--- sources/nvim/.config/nvim/lua/config/keymaps.lua
+++ sources/nvim/.config/nvim/lua/config/keymaps.lua
@@ -5,3 +5,7 @@
 -- Set H/L to first/last on line non-whitespace jump
 vim.keymap.set("n", "H", "_", { noremap = true, silent = true })
 vim.keymap.set("n", "L", "g_", { noremap = true, silent = true })
+
+-- Bubble selection up/down
+vim.keymap.set("v", "J", ":m '>+1<cr>gv=gv", { silent = true })
+vim.keymap.set("v", "K", ":m '<-2<cr>gv=gv", { silent = true })
