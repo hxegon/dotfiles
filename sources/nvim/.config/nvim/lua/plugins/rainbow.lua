1fe7bdfa66b6c8a7c2c843b09c181391b3294d29
Cooper LeBrun <cooperlebrun@gmail.com>
2024-05-31 16:23:11 -0400

    Add rainbow delimiters nvim plugin

---
+++ sources/nvim/.config/nvim/lua/plugins/rainbow.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "hiphish/rainbow-delimiters.nvim",
+    ft = { "clojure" },
+    setup = function()
+      require("rainbow-delimiters.setup").setup {
+        strategy = {
+          [''] = rainbow_delimiters.strategy['global'],
+          vim = rainbow_delimiters.strategy['local'],
+        },
+        query = {
+          [''] = 'rainbow-delimiters',
+          lua = 'rainbow-blocks',
+        },
+        highlight = {
+          'RainbowDelimiterRed',
+          'RainbowDelimiterYellow',
+          'RainbowDelimiterBlue',
+          'RainbowDelimiterOrange',
+          'RainbowDelimiterGreen',
+          'RainbowDelimiterViolet',
+          'RainbowDelimiterCyan',
+        }
+      }
+    end,
+  }
+}
