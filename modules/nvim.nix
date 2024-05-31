576d21058d43f52255060b67f218720d2950b359
Cooper LeBrun <cooperlebrun@gmail.com>
2024-05-31 16:23:23 -0400

    Add nvim dependency: unzip

--- modules/nvim.nix
+++ modules/nvim.nix
@@ -1,5 +1,6 @@
 { config, lib, pkgs, ... }:
 
+# Using steam-run for nvim may or may not be necessary. Sometimes nvim only works with steam-run, sometimes it won't work in it.
 {
   home.packages = with pkgs; [
     steam-run # required to give nvim/mason.nvim a more normal environment
@@ -8,6 +9,7 @@
     # plugin build dependencies
     gnumake
     gcc
+    unzip
     
     kitty # Terminal with truecolor and undercurl support
   ];
@@ -30,6 +32,8 @@
   };
 
   home.shellAliases = {
-    ni = "steam-run nvim";
+    # ni = "steam-run nvim";
+    ni = "nvim";
+    sni = "steam-run nvim";
   };
 }
