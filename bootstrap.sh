df5ed398d24c389da8f94adee72d4d878388fa0d
Cooper LeBrun <cooperlebrun@gmail.com>
2025-03-06 08:45:23 -0500

    Add bootstrap script (need to test on new system)

---
+++ bootstrap.sh
@@ -0,0 +1,20 @@
+# Based off of some info from https://www.chrisportela.com/posts/home-manager-flake/
+
+# Install nix
+echo "Installing nix as single user"
+# https://nixos.org/download/
+sh <(curl -L https://nixos.org/nix/install) --no-daemon
+
+# Enable nix flakes feature
+echo "Enabling flakes"
+mkdir -p ~/.config/nix
+echo "experimental-features = nix-command flakes" >>~/.config/nix/nix.conf
+
+# TODO: Ask for conf that initially loaded modules have been reviewed, abort if not
+
+# Initial nix run
+echo "Bootstrapping nix. THIS IS UNTESTED"
+nix run . -- build --flake .
+home-manager switch --flake .
+
+# TODO: Optional cloning of scripts into ~/bin
