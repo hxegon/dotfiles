46d5b966af44dc8b87ad2d1e71a44800e99b41f0
Cooper LeBrun <cooperlebrun@gmail.com>
2025-03-06 12:14:27 -0500

    use conditional config for ghostty

--- modules/ghostty.nix
+++ modules/ghostty.nix
@@ -1,11 +1,15 @@
-{ config, lib, pkgs, nixGL, ... }:
+{ config, lib, pkgs, nixGL, setup, ... }:
 
 {
   programs.ghostty = {
     enable = true;
+    # Necessary to wrap ghostty on non-nixOS systems
     package = (config.lib.nixGL.wrap pkgs.ghostty);
     enableBashIntegration = true;
-    enableZshIntegration = true;
+    enableZshIntegration = (if setup.shell == "zsh" then true else false);
     installBatSyntax = true;
+    settings = {
+      command = "/home/${setup.username}/.nix-profile/bin/${setup.shell}";
+    };
   };
 }
