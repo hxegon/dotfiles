f54f9acec190e899556870619043a1fd1c55f579
Cooper LeBrun <cooperlebrun@gmail.com>
2025-03-07 15:13:37 -0500

    add mini align and animate

--- modules/nvf.nix
+++ modules/nvf.nix
@@ -17,22 +17,22 @@
       };
 
       theme = {
-        enable = true;
-        name = "catppuccin";
-        style = "mocha";
+        enable      = true;
+        name        = "catppuccin";
+        style       = "mocha";
         transparent = false;
       };
 
       binds = {
-        whichKey.enable = true;
+        whichKey.enable   = true;
         cheatsheet.enable = true;
       };
 
       telescope.enable = true;
 
       git = {
-        enable = true;
-        gitsigns.enable = true;
+        enable                      = true;
+        gitsigns.enable             = true;
         gitsigns.codeActions.enable = false;
       };
 
@@ -43,10 +43,10 @@
         };
       };
 
-      vimAlias = true;
-      spellcheck.enable = true;
+      vimAlias                     = true;
+      spellcheck.enable            = true;
       autocomplete.nvim-cmp.enable = true;
-      notify.nvim-notify.enable = true;
+      notify.nvim-notify.enable    = true;
       projects.project-nvim.enable = true;
       comments.comment-nvim.enable = true;
 
@@ -55,8 +55,10 @@
         # sd" -> delete surrounding "
         # saiw" -> add " to inner word
         # sr{[ -> replace surrounding {} with []
+        animate.enable  = true;
+        pairs.enable    = true;
         surround.enable = true;
-        # indentscope.enable = true;
+        align.enable    = true;
       };
 
       navigation = {
@@ -65,11 +67,11 @@
 
           mappings = {
             listMarks = "<leader>h";
-            markFile = "<leader>H";
-            file1 = "<leader>1";
-            file2 = "<leader>2";
-            file3 = "<leader>3";
-            file4 = "<leader>4";
+            markFile  = "<leader>H";
+            file1     = "<leader>1";
+            file2     = "<leader>2";
+            file3     = "<leader>3";
+            file4     = "<leader>4";
           };
 
           setupOpts.defaults = {
@@ -95,42 +97,42 @@
 
       ui = {
         borders.enable = true;
-        noice.enable = true;
+        noice.enable   = true;
       };
 
       lsp = {
-        formatOnSave = true;
-        lspkind.enable = false;
-        lightbulb.enable = true;
-        lspsaga.enable = false;
-        trouble.enable = true;
-        lspSignature.enable = true;
-        otter-nvim.enable = true;
-        lsplines.enable = true;
+        formatOnSave          = true;
+        lspkind.enable        = false;
+        lightbulb.enable      = true;
+        lspsaga.enable        = false;
+        trouble.enable        = true;
+        lspSignature.enable   = true;
+        otter-nvim.enable     = true;
+        lsplines.enable       = true;
         nvim-docs-view.enable = true;
       };
 
       # TODO: enable languages based on setup var
       languages = {
-        enableLSP = true;
-        enableFormat = false;
-        enableTreesitter = true;
+        enableLSP              = true;
+        enableFormat           = false;
+        enableTreesitter       = true;
         enableExtraDiagnostics = true;
 
         # Always should be enabled
-        nix.enable = true;
-        bash.enable = true;
-        markdown.enable = true;
+        nix.enable             = true;
+        bash.enable            = true;
+        markdown.enable        = true;
 
-        css.enable = lib.mkIf (builtins.elem "web" setup.languages) true;
-        html.enable = lib.mkIf (builtins.elem "web" setup.languages) true;
-        ts.enable = lib.mkIf (builtins.elem "web" setup.languages) true;
+        css.enable             = lib.mkIf (builtins.elem "web" setup.languages) true;
+        html.enable            = lib.mkIf (builtins.elem "web" setup.languages) true;
+        ts.enable              = lib.mkIf (builtins.elem "web" setup.languages) true;
 
-        lua.enable = lib.mkIf (builtins.elem "lua" setup.languages) true;
-        python.enable = lib.mkIf (builtins.elem "python" setup.languages) true;
-        go.enable = lib.mkIf (builtins.elem "go" setup.languages) true;
-        nu.enable = lib.mkIf (builtins.elem "nu" setup.languages) true;
-        ruby.enable = lib.mkIf (builtins.elem "ruby" setup.languages) true;
+        lua.enable             = lib.mkIf (builtins.elem "lua" setup.languages) true;
+        python.enable          = lib.mkIf (builtins.elem "python" setup.languages) true;
+        go.enable              = lib.mkIf (builtins.elem "go" setup.languages) true;
+        nu.enable              = lib.mkIf (builtins.elem "nu" setup.languages) true;
+        ruby.enable            = lib.mkIf (builtins.elem "ruby" setup.languages) true;
       };
 
       treesitter = {
@@ -144,25 +146,25 @@
 
       keymaps = [
         {
-          key = "H";
+          key    = "H";
           action = "_";
-          mode = "n";
+          mode   = "n";
           silent = true;
-          lua = false;
+          lua    = false;
         }
         {
-          key = "L";
+          key    = "L";
           action = "g_";
-          mode = "n";
+          mode   = "n";
           silent = true;
-          lua = false;
+          lua    = false;
         }
         {
-          key = "<leader>f<leader>";
+          key    = "<leader>f<leader>";
           action = "require('telescope.builtin').git_files";
-          mode = "n";
+          mode   = "n";
           silent = true;
-          lua = true;
+          lua    = true;
         }
       ];
 
