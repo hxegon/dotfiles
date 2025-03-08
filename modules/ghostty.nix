1e614ea00f6d13dfe5169280808431dad9c3bc34
Cooper LeBrun <cooperlebrun@gmail.com>
2025-03-08 16:44:45 -0500

    ghostty: change cursor size, disable custom font b/c issues

--- modules/ghostty.nix
+++ modules/ghostty.nix
@@ -1,18 +1,33 @@
-{ config, lib, pkgs, nixGL, setup, ... }:
-
 {
+  config,
+  pkgs,
+  setup,
+  ...
+}: {
   programs.ghostty = {
     enable = true;
     # Necessary to wrap ghostty on non-nixOS systems
-    package = (config.lib.nixGL.wrap pkgs.ghostty);
-    enableBashIntegration = true;
-    enableZshIntegration = (if setup.shell == "zsh" then true else false);
+    package = config.lib.nixGL.wrap pkgs.ghostty;
+
+    enableBashIntegration = true; # Sometimes need to fall back to bash
+
+    enableZshIntegration =
+      if setup.shell == "zsh"
+      then true
+      else false;
+
     installBatSyntax = true;
+
     settings = {
       theme = "catppuccin-mocha";
       command = "/home/${setup.username}/.nix-profile/bin/${setup.shell}";
-      font-family = "Mononoki Nerd Font Mono";
+      # nerd font / unicode stuff not working with this?
+      # font-family = "Mononoki Nerd Font Mono";
       font-size = 12;
+      cursor-style = "block";
+
+      # Necessary for cursor-style to work
+      shell-integration-features = "no-cursor";
     };
   };
 }
