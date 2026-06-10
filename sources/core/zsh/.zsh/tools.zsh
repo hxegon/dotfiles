4a6e564798f0832a5436a881752420cc6d24d744
Cooper LeBrun <cooperlebrun@gmail.com>
2026-06-10 10:48:14 -0400

    Add env vars for AISHELL provider/model, switch to using deepseek platform over openrouter

--- sources/core/zsh/.zsh/tools.zsh
+++ sources/core/zsh/.zsh/tools.zsh
@@ -12,3 +12,8 @@ export FZF_CTRL_T_COMMAND='rg --files --hidden'
 export FZF_DEFAULT_OPTS="--ansi --reverse --border -m --preview='bat --color=\"always\" {}'"
 
 export BAT_THEME="zenburn"
+
+# aishell env vars. These match the defaults in the script but are put here for redundency
+export AISHELL_PROVIDER=deepseek
+export AISHELL_DEFAULT_MODEL=deepseek-v4-flash
+export AISHELL_EXTRA_MODEL=deepseek-v4-pro
